3bcfceca69cff177b9a9996810906b0a2a887c03
Volker Stoppe <volker@stoppe.de>
2021-09-21 13:17:05 +0200

    new host

--- k3s-m1-install.sh
+++ k3s-m1-install.sh
@@ -11,14 +11,13 @@ DATA_DISK="$VIRT_DIR/${NAME}_data.qcow2"
 #### Configure kernel args for installation
 SILENT_INST=true # Ensure no questions will be asked
 POWER_OFF=true    # Power off after install
-CONFIG_URL="http://infraweb.lindenbox.de/$NAME.yaml"
+CONFIG_URL="http://infraweb.linden.net/$NAME.yaml"
 KERNEL_ARGS="k3os.token=$SECRET console=ttyS0 k3os.install.tty=ttyS0 k3os.install.device=/dev/vda k3os.mode=install k3os.install.silent=$SILENT_INST k3os.install.power_off=$POWER_OFF k3os.install.config_url=$CONFIG_URL k3os.install.iso_url=$URL_ISO"
 
 
 # Cleanup old artefacts
 virsh destroy $NAME
-virsh undefine $NAME
-rm -f $VIRT_DIR/$NAME.qcow2
+virsh undefine $NAME --remove-all-storage
 
 
 virt-install -v \
@@ -27,11 +26,11 @@ virt-install -v \
 	--boot kernel=$KERNEL,initrd="$INITRD",kernel_args="$KERNEL_ARGS"  \
 	--vcpu $CPU \
 	--memory $(($RAM*1024)) \
-	--disk $DISK,     bus=virtio,size=$DISK_SIZE,     format=qcow2 \
+	--disk $DISK,bus=virtio,size=$DISK_SIZE,format=qcow2 \
 	--disk $DATA_DISK,bus=virtio,size=$DATA_DISK_SIZE,format=qcow2 \
 	--cdrom $ISO \
-	--network=bridge,model=virtio,mac=52:54:00:D0:33:DA \
+	--network network=bridged-network,model=virtio,mac=52:54:00:D0:33:DA \
+	--os-variant "ubuntu18.04" \
 	--os-type linux \
-	--os-variant ubuntu20.04 \
 	--graphics none \
 	--console pty,target_type=serial
